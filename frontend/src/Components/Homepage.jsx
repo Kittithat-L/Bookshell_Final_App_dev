da7a4ba9732feb82c40b52a0ae463ca5f022f2a0
jirayut011 <jirayut.jojo1@gmail.com>
2025-03-22 16:56:04 +0700

    update tailwind css to Homepage

--- frontend/src/Components/Homepage.jsx
+++ frontend/src/Components/Homepage.jsx
@@ -8,32 +8,40 @@ const HomePage = () => {
     navigate('/login');
   }
   return (
-    <div className="container">
-    <aside className="sidebar">
-      <h1 className="logo">BOOKSHELL</h1>
-      <nav>
-        <button className="nav-btn home-btn">🏠 HOME</button>
-        <button className="nav-btn search-btn">🔍 SEARCH</button>
-      </nav>
-      <div className="shelves">
-        <h2>#MY SHELVES</h2>
-        <button className="shelf-btn">MY LIBRARY</button>
-        <button className="shelf-btn">FAVORITES</button>
+    <div className='flex flex-row justify-between items-center w-screen'>
+      <div className=" containter flex overflow-hidden rounded-[10px] bg-white w-[20%] min-h-screen border-r-[6px] mr-1">
+        <aside className="sidebar text-black min-w-[250px] px-[20px] py-[20px] flex flex-col justify-between">
+          <h1 className="logo text-center font-bold text-[1.8em]">BOOKSHELL</h1>
+          <nav>
+            <button className="nav-btn home-btn w-[100%] p-[12px] my-[10px] border-[2px] rounded-[20px] bg-[#ffcbbf] cursor-pointer font-[1em] flex items-center justify-center">🏠 HOME</button>
+            <button className="nav-btn search-btn w-[100%] p-[12px] my-[10px] border-[2px] rounded-[20px] bg-[#ffcbbf] cursor-pointer font-[1em] flex items-center justify-center">🔍 SEARCH</button>
+          </nav>
+          <div className="shelves mt-[20px]">
+            <h2>#MY SHELVES</h2>
+            <button className="shelf-btn w-[100%] p-[12px] my-[10px] border-[2px] rounded-[20px] bg-[#ffcbbf] cursor-pointer font-[1em] flex items-center justify-center">MY LIBRARY</button>
+            <button className="shelf-btn w-[100%] p-[12px] my-[10px] border-[2px] rounded-[20px] bg-[#ffcbbf] cursor-pointer font-[1em] flex items-center justify-center">FAVORITES</button>
+          </div>
+          <div className="account-section mt-auto">
+            <button className="account-btn w-[100%] p-[12px] my-[10px] border-[2px] rounded-[20px] bg-[#ffcbbf] cursor-pointer font-[1em] flex items-center justify-center">👤 My account</button>
+            <button className="account-btn w-[100%] p-[12px] my-[10px] border-[2px] rounded-[20px] bg-[#ffcbbf] cursor-pointer font-[1em] flex items-center justify-center">❓ Help</button>
+            <button className="account-btn w-[100%] p-[12px] my-[10px] border-[2px] rounded-[20px] bg-[#ffcbbf] cursor-pointer font-[1em] flex items-center justify-center">📖 Feedback</button>
+          </div>
+        </aside>
       </div>
-      <div className="account-section">
-        <button className="account-btn">👤 My account</button>
-        <button className="account-btn">❓ Help</button>
-        <button className="account-btn">📖 Feedback</button>
+
+
+      <div className=' containter flex overflow-hidden rounded-[10px] bg-white w-[70%] min-w-[50vh] min-h-screen mx-auto border-r-[6px]'>
+      <main className="content flex-1 p-[20px] bg-cover flex-col items-center content-start w-100%">
+        <div className="search-bar flex columns-2 w-[100%] bg-[#ffcbbf] rounded-[30px] p-[5px] items-center border-2">
+          <input type="text" placeholder="Search..." className=' flex border-0 bg-transparent p-[10px] text-[1.2em] outline-none w-[90%]' />
+          <button className='border-0 bg-transparent cursor-pointer text-[1.2em] w-[10%]'>🔍</button>
+        </div>
+        <div className=" blo">
+          content
+        </div>
+      </main>
       </div>
-    </aside>
-    <main className="content">
-      <div className="search-bar">
-        <input type="text" placeholder="Search..." />
-        <button>🔍</button>
-      </div>
-      <div className=" blo"></div>
-    </main>
-  </div>
+    </div>
 );
 }
 export default HomePage
